f12c293533f89bcda3d664037b9665cf68893dab
Henning.Sperr <henningsperr@gmail.com>
2013-07-10 13:59:02 +0200

    README update Removed Logging - avoid unecessary dependencies

--- Speechrecognizer/src/hiro/SpeechRecognitionFramework.java
+++ Speechrecognizer/src/hiro/SpeechRecognitionFramework.java
@@ -9,21 +9,15 @@ import hiro.window.WindowFramer;
 import java.io.IOException;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class SpeechRecognitionFramework implements ISpeechRecognition {
-
-	static final Logger logger = LoggerFactory
-			.getLogger(SpeechRecognitionFramework.class);
 
 	public static void main(String[] args) throws IOException,
 			InterruptedException {
 
-		logger.info("Hiro speech recognizer!! yay V0.1");
+		System.out.println("Hiro speech recognizer!! yay V0.1");
 
 		ISpeechRecognition srfw = new SpeechRecognitionFramework();
-		logger.info(srfw.getTextFroMic());
+		System.out.println(srfw.getTextFroMic());
 
 	}
 
@@ -36,7 +30,7 @@ public class SpeechRecognitionFramework implements ISpeechRecognition {
 
 		audioRecorder.init();
 		audioRecorder.startRecording();
-		logger.info("Recording, press key to stop!");
+		System.out.println("Recording, press key to stop!");
 		try {
 			System.in.read();
 		} catch (IOException e) {
@@ -46,7 +40,7 @@ public class SpeechRecognitionFramework implements ISpeechRecognition {
 
 		audioRecorder.stopRecording();
 
-		logger.info("Stopped Recording with: "
+		System.out.println("Stopped Recording with: "
 				+ audioRecorder.getSoundData().size() + " samples");
 
 		List<List<Double>> windowedSignal = windowFramer.applyWindow(
